26d0150866e51178de990929afd6a2973e53a147
Aman Patel <57772408+aman9824@users.noreply.github.com>
2020-09-21 03:10:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -51,7 +51,6 @@ app.post("/", function(req, res) {
 
   request.write(jsonData);
   request.end();
-  // app.send("<h1> firstName : " + firstName + "<br> lastName : " + lastName + "<br> email: " + email + "</h1>");
 });
 
 app.post("/failure", function(req, res) {
@@ -61,7 +60,3 @@ app.post("/failure", function(req, res) {
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server is running");
 });
-
-//API Key: cb411cab26cd94eab14a914898854f01-us2
-//List id: 0f98bb942d
-//   'https://server.api.mailchimp.com/3.0/lists?fields=<SOME_ARRAY_VALUE>&exclude_fields=<SOME_ARRAY_VALUE>&count=10&offset=0&before_date_created=<SOME_STRING_VALUE>&since_date_created=<SOME_STRING_VALUE>&before_campaign_last_sent=<SOME_STRING_VALUE>&since_campaign_last_sent=<SOME_STRING_VALUE>&email=<SOME_STRING_VALUE>&sort_field=<SOME_STRING_VALUE>&sort_dir=<SOME_STRING_VALUE>' \
